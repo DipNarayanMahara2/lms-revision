a8d3045e2b92c6f4a454c48a3523bb0e7caf240f
DipNarayanMahara2 <dipnarayanmahara28@gmail.com>
2025-02-27 21:43:34 +0545

    this is begging

---
+++ lms/src/app/about/page.tsx
@@ -0,0 +1,5 @@
+function about() {
+  return <h1>this is about page</h1>;
+}
+
+export default about;
---
+++ lms/src/app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import home from "./Home/page";
+
+export default function Home() {
+  return <h1>home</h1>;
+}
